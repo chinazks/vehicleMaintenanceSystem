4d29b7ec9a81c0ef55e10d4418086ef160ce75c9
chinazks <1324386057@qq.com>
2018-05-30 16:56:06 +0800

    unitinformationdao

--- vehicleMaintenanceSystem/src/main/java/com/xforceplus/data/dao/UnitInformationRepository.java
+++ vehicleMaintenanceSystem/src/main/java/com/xforceplus/data/dao/UnitInformationRepository.java
@@ -1,8 +1,6 @@
 package com.xforceplus.data.dao;
 
 import com.xforceplus.data.bean.UnitInformation;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
@@ -10,10 +8,8 @@ import org.springframework.stereotype.Repository;
 /**
  * Created by Administrator on 2018/5/5 0005.
  */
-@Repository
+@Repository("UnitInformationRepository")
 public interface UnitInformationRepository extends JpaRepository<UnitInformation,Long>, JpaSpecificationExecutor<UnitInformation>{
-    Page<UnitInformation> findAll(Pageable pageable);
-    Page<UnitInformation> findByUnitIdLike(String unitId,Pageable pageable);
-
-    
+   // Page<UnitInformation> findAll(Pageable pageable);
+   // Page<UnitInformation> findByUnitIdLike(String unitId,Pageable pageable);   
 }
